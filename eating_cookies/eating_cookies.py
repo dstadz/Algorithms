ed6468cc1444709c18cc1bbf5554205d1f0742a2
Daniel <danstad2012@gmail.com>
2020-03-18 15:56:09 -0700

    rps and cookies not done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,24 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+ways = 0
 def eating_cookies(n, cache=None):
-  pass
+  print(n, ways)
+  if n == 0:
+    return (ways + 1)
+  elif n == 1:
+    return (ways + 1)
+  elif n == 2:
+    return (ways + 2)
+  elif n == 3:
+    return (ways + 4)
+  
+  eating_cookies(n-1)
+  eating_cookies(n-2)
+  eating_cookies(n-3)
+
+
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -14,3 +30,14 @@ if __name__ == "__main__":
     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
     print('Usage: eating_cookies.py [num_cookies]')
+
+
+    # (0), 1)
+    # (1), 1)
+    # (2), 2)
+    # (5), 13)
+    # (10), 274)
+
+    # (50, [0 for i in range(51)]), 10562230626642)
+    # (100, [0 for i in range(101)]), 180396380815100901214157639)
+    # (500, [0 for i in range(501)]), 1306186569702186634983475450062372018715120191391192207156664343051610913971927959744519676992404852130396504615663042713312314219527)
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,14 @@
 
 import sys
 
+rps = [['rock'], ['paper'], ['scissors']]
 def rock_paper_scissors(n):
-  pass 
+  if n == 0:
+    return []
+  if n == 1:
+    return [['rock'], ['paper'], ['scissors']]
 
+    
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
